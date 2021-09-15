2ac2a41b0b729e0070e6f1dc7516a38146f22e07
Brent <76509483+Brent-prod@users.noreply.github.com>
2021-09-15 22:52:01 +1000

    completed but needs polishing

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,6 +1,18 @@
 import './intro.scss'
+import { init } from "ityped"
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+   const textRef = useRef()
+
+   useEffect(() => {
+      init(textRef.current, {
+         showCursor: true, // built in settings from iTyped library
+         backDelay: 1500, 
+         backSpeed: 60,
+         strings: ['Engineer', 'Developer', 'Creator' ] })
+   }, [])
+
    return (
       <div className='intro' id='intro'>
          <div className="left">
@@ -12,7 +24,7 @@ export default function Intro() {
             <div className="wrapper">
                <h2>Hi There, I'm</h2>
                <h1>Silvio Brentan</h1>
-               <h3>Software<span> Engineer</span></h3>
+               <h3>Software <span ref={textRef}></span></h3>
             </div>
             <a href="#portfolio">
                <img src="assets/arrowDown.jpeg" alt="" / // need to change this for material-ui icon
