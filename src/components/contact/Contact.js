c88e424a0b0365fda93dafdd19aad21071b0c3ef
Brent <76509483+Brent-prod@users.noreply.github.com>
2021-09-23 18:54:14 +1000

    fixed form to google and form to hide

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,19 +3,20 @@ import { useState } from 'react';
 import postToGoogle from '../../submitToGoogle';
 
 export default function Contact() {
-	const [ message, setMessage ] = useState(false);
+	const [ message, setMessage ] = useState(false); // this to change the confirmation message
+	const [ hideForm, setForm ] = useState(false); // this is to hide the form
 
 	const handleSubmit = (e) => {
 		e.preventDefault(); // this it to prevent the page to reload when hitting the send button
 		setMessage(true);
 		postToGoogle()
-		.then(response => {
-			// how to reset the form
-		})
-		.catch(err =>{
-			// look up possible err msg to be displayed e.g. ERROR
-		})
-
+			.then((response) => {
+				// how to reset the form
+			})
+			.catch((err) => {
+				// look up possible err msg to be displayed e.g. ERROR
+			});
+		setForm(true);
 	};
 	return (
 		<div className="contact" id="contact">
@@ -26,45 +27,19 @@ export default function Contact() {
 				/>
 			</div>
 			<div className="right">
-				<h2>Contact Me.</h2>
+				{!hideForm && 
+				<><h2>Contact Me.</h2>
 				<form onSubmit={handleSubmit}>
-					<input
-						placeholder="Your Name"
-						type="text"
-						data-name="Name"
-						id="Name"
-						required
-						autoFocus
-					/>
-					<input
-						placeholder="Your Email Address"
-						type="email"
-						data-name="Email"
-						id="Email"
-						required
-					/>
-					<input
-						placeholder="Your Phone Number (optional)"
-						type="tel"
-						data-name="Phone"
-						id="Phone"
-					/>
-					<input
-						placeholder="Your Web Site (optional)"
-						type="url"
-						data-name="Website"
-						id="Website"
-					/>
-					<textarea
-						placeholder="Type your message here...."
-						data-name="Message"
-						id="Message"
-						required
-					/>
-					<button type="submit">Send</button>
-					{message && <span>Thanks, I'll reply ASAP :)</span> // this will happen only after the state has been set to true
-					}
-				</form>
+						<input placeholder="Your Name" type="text" data-name="Name" id="Name" required />
+						<input placeholder="Your Email Address" type="email" data-name="Email" id="Email" required />
+						<input placeholder="Your Phone Number (optional)" type="tel" data-name="Phone" id="Phone" />
+						<input placeholder="Your Web Site (optional)" data-name="Website" id="Website" />
+						<textarea placeholder="Type your message here...." data-name="Message" id="Message" required />
+						<button type="submit">Send</button>
+					</form></>
+				}
+				{message && <span>Thanks, I'll reply ASAP :)</span> // this will happen only after the state has been set to true
+				}
 			</div>
 		</div>
 	);
