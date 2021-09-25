366e341fd8ca48f2a5a0f645dd08efc80aa4f1b5
Brent <76509483+Brent-prod@users.noreply.github.com>
2021-09-25 12:54:15 +1000

    changed color schema

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -24,7 +24,7 @@ export default function Intro() {
 			<div className="right">
 				<div className="wrapper">
 					<h2>Hi There, I'm</h2>
-					<h1>Silvio Brentan</h1>
+					<h1>Brent.</h1>
 					<h3>
 						Software <span ref={textRef} />
 					</h3>
