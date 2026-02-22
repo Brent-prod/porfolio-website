a23ad75de737b36382ebe0aa4fb03c3e97cbedfd
Brent <76509483+Brent-prod@users.noreply.github.com>
2026-02-23 09:29:21 +1100

    fixed google contact me form

--- src/submitToGoogle.js
+++ src/submitToGoogle.js
@@ -1,18 +1,19 @@
-import axios from 'axios';
-
 function postToGoogle({ name, email, phone, website, message }) {
-	return axios({
-		url:
-			'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeDqYgnfj4oRbe2BxHo9CyJpAjDamuLOUt_gO7p8-bFOtsfTw/formResponse',
-		params: {
-			'entry.150345244': name,
-			'entry.751255685': email,
-			'entry.127353653': phone,
-			'entry.1892483773': website,
-			'entry.1524902182': message
-		},
-		method: 'post'
-	});
+	const formData = new FormData();
+	formData.append('entry.150345244', name);
+	formData.append('entry.751255685', email);
+	formData.append('entry.127353653', phone);
+	formData.append('entry.1892483773', website);
+	formData.append('entry.1524902182', message);
+
+	return fetch(
+		'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeDqYgnfj4oRbe2BxHo9CyJpAjDamuLOUt_gO7p8-bFOtsfTw/formResponse',
+		{
+			method: 'POST',
+			mode: 'no-cors',
+			body: formData
+		}
+	);
 }
 
 export default postToGoogle;
